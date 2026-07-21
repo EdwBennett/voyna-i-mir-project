3eea3e71c863772aac1fff05937355a3396a4d53
Ed Bennett <edw.bennett@gmail.com>
2026-07-21 04:55:51 -0500

    1st sentence added

--- core/create_sentence_db.py
+++ core/create_sentence_db.py
@@ -7,6 +7,7 @@ class Voyna_I_Mir(SQLModel, table=True):
     chapter_sequence: int
     chapter_full_name: int
     voyna_i_mir_russian: str = Field(sa_type=AutoString(length=1000))
+    russian_transliteration: str = Field(sa_type=AutoString(length=1000))
     english_translation: str = Field(sa_type=AutoString(length=1000))
 
 
