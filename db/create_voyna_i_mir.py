93f2fcb4d8fbe89a260057c7db54048609ff10e6
Ed Bennett <edw.bennett@gmail.com>
2026-07-21 21:54:45 -0500

    add a sentence data

--- db/create_voyna_i_mir.py
+++ db/create_voyna_i_mir.py
@@ -5,7 +5,7 @@ from sqlmodel.sql.sqltypes import AutoString
 class Voyna_I_Mir(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     chapter_sequence: int
-    chapter_full_name: int
+    chapter_full_name: str = Field(sa_type=AutoString(length=50))
     voyna_i_mir_russian: str = Field(sa_type=AutoString(length=1000))
     russian_transliteration: str = Field(sa_type=AutoString(length=1000))
     english_translation: str = Field(sa_type=AutoString(length=1000))
