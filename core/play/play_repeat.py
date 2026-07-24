c40627d30957e114ba92f8566ac3eb7a6c9001d3
Ed Bennett <edw.bennett@gmail.com>
2026-07-24 04:35:10 -0500

    call play_repeat(...) from main

--- core/play/play_repeat.py
+++ core/play/play_repeat.py
@@ -1,4 +1,6 @@
+import argparse
 import sys
+import time
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
@@ -9,22 +11,42 @@ from sqlmodel import Session, create_engine, select
 from db.models import Voyna_I_Mir
 from core.say_sentence.say import say
 
-db_path = PROJECT_ROOT / "db" / "voyna_i_mir.sqlite"
-engine = create_engine(f"sqlite:///{db_path}")
+def play_repeat(*, chapter: int, count: int, delay: int) -> None:
+
+    db_path = PROJECT_ROOT / "db" / "voyna_i_mir.sqlite"
+    engine = create_engine(f"sqlite:///{db_path}")
+
+    with Session(engine) as session:
+        statement = (
+            select(Voyna_I_Mir)
+            .where(Voyna_I_Mir.chapter_sequence == chapter)
+            .limit(1)
+        )
+        record = session.exec(statement).first()
+
+    if record is None:
+        print("No record found")
+        return
+
+    for repetition in range(count):
+        print()
+        print(record.voyna_i_mir_russian, flush=True, end="\n\n")
+        print(record.russian_transliteration, flush=True, end="\n\n")
+        say(lang="ru", text=record.voyna_i_mir_russian)
+        time.sleep(delay)
+        print(record.english_translation, flush=True, end="\n\n")
+        time.sleep(3)
 
 
-with Session(engine) as session:
-    statement = (
-        select(Voyna_I_Mir)
-        .where(Voyna_I_Mir.chapter_sequence == 1)
-        .limit(1)
-    )
-    record = session.exec(statement).first()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="play on repeat script.")
+    parser.add_argument("-c", "--chapter", type=int, default=1, help="Select Chapter")
+    parser.add_argument("-n", "--count", type=int, default=2, help="Select Count")
+    parser.add_argument("-d", "--delay", type=int, default=3, help="Select Delay")
+    args = parser.parse_args()
 
-
-if record is None:
-    print("No record found")
-else:
-#    print(record.model_dump_json(indent=2))
-    print(record.voyna_i_mir_russian, flush=True)
-    say(lang="ru", text=record.voyna_i_mir_russian)
+    try:
+        play_repeat(chapter=args.chapter, count=args.count, delay=args.delay)
+    except Exception as error:
+        print(f"Error: {error}", file=sys.stderr)
+        sys.exit(1)
