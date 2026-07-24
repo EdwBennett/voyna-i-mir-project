2d7bcc47c0d424d984126b13dfd7b84e8dcbe176
Ed Bennett <edw.bennett@gmail.com>
2026-07-24 03:19:21 -0500

    remove tmp scrpts

--- core/play/helper.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine, inspect
-
-db_path = "/home/edbennett/Documents/projects/voyna-i-mir-project/db/voyna_i_mir.sqlite"
-engine = create_engine(f"sqlite:///{db_path}")
-
-print(inspect(engine).get_table_names())
