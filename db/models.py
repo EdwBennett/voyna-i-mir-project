6f2c94c43bdd50895c29e910aac4a5734a4f7dc3
Ed Bennett <edw.bennett@gmail.com>
2026-07-29 13:55:10 -0500

    changed stropes to JSON

--- db/models.py
+++ db/models.py
@@ -1,11 +1,16 @@
 # models.py
-from sqlmodel import SQLModel, Field
+from sqlalchemy import CheckConstraint, Column
+from sqlmodel import JSON, SQLModel, Field
 
 class Voyna_I_Mir(SQLModel, table=True):
+    __table_args__ = (
+        CheckConstraint("json_valid(english_strophes)", name="ck_english_strophes_json"),
+    )
+
     id: int | None = Field(default=None, primary_key=True)
     chapter_sequence: int
     chapter_full_name: str = Field(max_length=50)
     voyna_i_mir_russian: str = Field(max_length=1000)
     russian_transliteration: str = Field(max_length=1000)
     english_translation: str = Field(max_length=1000)
-    english_strophes: str | None = Field(default=None, max_length=1000)
+    english_strophes: dict | None = Field(default=None, sa_column=Column(JSON))
